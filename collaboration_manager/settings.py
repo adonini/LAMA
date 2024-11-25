f0aaf209ff3e4523a73aed1460b4e28fa37e094d
Antonio J.Penuela-Lopez <antonio.penuela@cta-consortium.org>
2024-11-25 19:57:48 +0000

    small changes

--- collaboration_manager/settings.py
+++ collaboration_manager/settings.py
@@ -18,7 +18,7 @@ from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
 
 # Load environment variables from .env file
-load_dotenv('/../.env')
+load_dotenv('/code/.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
