9395d961b18d0e3f63e479bc85901afee31af61d
Alice Donini <alixd90@gmail.com>
2025-01-13 17:31:25 +0000

    Add memberadmin model with searchbar

--- members/admin.py
+++ members/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import ActiveDutyManager, Country, Duty, Group, Institute, Member, MemberDuty, MembershipPeriod, AuthorshipPeriod
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname'] # Fields to display in the list view
+    search_fields = ['name', 'surname']  # Fields to search by
+    list_filter = ['name', 'surname']
+
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Group)
 admin.site.register(Institute)
 admin.site.register(Duty)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(MemberDuty)
 admin.site.register(MembershipPeriod)
 admin.site.register(AuthorshipPeriod)
