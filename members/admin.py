8cdac08354c2cea68192dfdef605c67dc2f6f32f
Alice Donini <alixd90@gmail.com>
2025-01-13 17:40:14 +0000

    add Member admin search

--- members/admin.py
+++ members/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import ActiveDutyManager, Country, Duty, Group, Institute, Member, MemberDuty, MembershipPeriod, AuthorshipPeriod, AuthorDetails
+from .models import Country, Duty, Group, Institute, Member, MemberDuty, MembershipPeriod, AuthorshipPeriod, AuthorDetails
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname'] # Fields to display in the list view
+    search_fields = ['name', 'surname']  # Fields to search by
+    list_filter = ['name', 'surname']
+
+
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Group)
 admin.site.register(Institute)
 admin.site.register(Duty)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(MemberDuty)
 admin.site.register(MembershipPeriod)
 admin.site.register(AuthorshipPeriod)
